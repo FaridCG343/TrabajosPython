21a5be056ceb4635425f2d044cd249881473193b
FaridCG343 <71570619+FaridCG343@users.noreply.github.com>
2022-10-18 00:29:51 -0600

    Update Manos.py

--- Manos.py
+++ Manos.py
@@ -1,6 +1,5 @@
 import cv2
 import mediapipe as mp
-import time
 
 
 class Manos:
@@ -8,39 +7,24 @@ class Manos:
         self.mpHands = mp.solutions.hands
         self.hands = self.mpHands.Hands(static_image_mode, max_num_hands, model_complexity, min_detection_confidence, min_tracking_confidence)
         self.mpDraw = mp.solutions.drawing_utils
-        self.ptime = 0
-        self.ctime = 0
 
-    def calcularFPS(self,img):
-        ctime = time.time()
-        fps = 1 / (ctime - self.ptime)
-        self.ptime = ctime
+    def findHands(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.result = self.hands.process(imgRGB)
+        if self.result.multi_hand_landmarks:
+            for handLms in self.result.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+        return img
 
-        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
-
-    def draw_landmarks(self, img, result):
-        for handLms in result.multi_hand_landmarks:
-
-            for id, lm in enumerate(handLms.landmark):
-
+    def getPos(self, img, nH=0, draw=True):
+        lmList = []
+        if self.result.multi_hand_landmarks:
+            for id, lm in enumerate(self.result.multi_hand_landmarks[nH].landmark):
                 h, w, c = img.shape
-
-                cx, cy = int(lm.x*w), int(lm.y*h)
-                if id == 4:
-                    cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
-            self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
-
-    def main(self):
-        cap = cv2.VideoCapture(0)
-        while True:
-            success, img = cap.read()
-            imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-            result = self.hands.process(imgRGB)
-            if result.multi_hand_landmarks:
-                self.draw_landmarks(img, result)
-            self.calcularFPS(img)
-            cv2.imshow("Image", img)
-            cv2.waitKey(1)
-
-
+                cx = int(lm.x*w)
+                cy = int(lm.y*h)
+                lmList.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
+        return lmList
